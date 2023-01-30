454db206aa4da768387f31bd54a5d865974d8cd9
juanj72 <juan.jara@campusucc.edu.co>
2023-01-30 10:19:56 -0500

    agregacion ejemplo de urls con clases y directamente con metodos

--- first_app/views.py
+++ first_app/views.py
@@ -6,4 +6,7 @@ def inicio(request):
     return HttpResponse("hola mundo desde mi nuevo cruso API")
 
 
+class ventanas_apis():
+    def modulo1(request):
+        return HttpResponse("este es una prueba desde un metodo de la clase ventanas_apis")
 
