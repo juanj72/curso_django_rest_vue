8fc54cb1dca7bf633dd21c0cfe314eec6d0bd7f6
juanj72 <juan.jara@campusucc.edu.co>
2023-01-31 16:12:01 -0500

    api de ejemplo con metodo serializado y sin serializar

--- first_app/urls.py
+++ first_app/urls.py
@@ -4,5 +4,8 @@ from first_app import views
 urlpatterns=[
     path('inicio',views.inicio,name='inicio'),
     path('inicio2',views.ventanas_apis.modulo1,name='modulo1'),
-    path('template',views.ventanas_apis.ventana_htm,name='template')
+    path('template',views.ventanas_apis.ventana_htm,name='template'),
+    path('post_api/<slug_v>',views.postDetailView.as_view()),
+    path('post_api2',views.First_app_view.as_view())
+
 ]
--- first_app/views.py
+++ first_app/views.py
@@ -1,18 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from django.http import HttpResponse
 import datetime as tm
 from django.http.response import JsonResponse as js
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import *
+from .serializers import *
 
 
 def inicio(request):
-    mensaje={
-    'name':'juan',
+    mensaje=[
+    {'name':'juan',
     'apellido':'jara',
-    'rol':'estudiante'
+    'rol':'estudiante'},
+    {'name':'jorge',
+    'apellido':'vargas',
+    'rol':'dev'}
 
-    }
+    ]
 
-    return js(mensaje)
+    return js(mensaje,safe=False)
 
 
 class ventanas_apis():
@@ -25,3 +32,22 @@ class ventanas_apis():
 
         return render(request,'index.html',{'mensaje':mensaje})
 
+
+
+class First_app_view(APIView):
+    def get(self,request,*args,**kwargs):
+        post_m=post.objects.all()
+        serializer=postSerializer(post_m,many=True)
+        return Response(serializer.data)
+
+
+class postDetailView(APIView):
+    def get(self,request,slug_v,*args,**kwargs):
+        if slug_v==None:
+            slug_v='1'
+        #post_m=post.objects.get(slug=slug_v)
+        post_m=get_object_or_404(post,slug=slug_v)
+        serializer=postSerializer(post_m)
+
+
+        return Response(serializer.data)
